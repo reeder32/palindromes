885ab1e0846aaa7678bd260ec584495073f2b53f
Nicholas Reeder <nickreeder32@gmail.com>
2021-05-04 10:13:41 -0700

    init files
    co-authored-by: Brandon Magofna <Magofna68@gmail.com>

---
+++ Palindrome.Tests/ModelTests/PalindromeTrackerTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Palindrome;
+
+namespace Palindrome.Tests
+{
+  [TestClass]
+  public class PalindromeTrackerTests
+  {
+    // Test methods go here
+  }
+}
---
+++ Palindrome/Models/PalindromeTracker.cs
@@ -0,0 +1,5 @@
+namespace Palindrome {
+  public class PalindromeTracker {
+    
+  }
+}
